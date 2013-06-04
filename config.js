2484eea5cd9ce61ba4d459d0253f3c08bb82adec
Steve Blackmon <sblackmon@w2ogroup.com>
2013-06-04 13:59:25 -0500

    config

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ var config = new Settings(
   // By default this will attempt to reach ES at the same host you have
   // elasticsearch installed on. You probably want to set it to the FQDN of your
   // elasticsearch host
-  elasticsearch:    "http://"+window.location.hostname+":9200",   
+  elasticsearch:    "http://es1.redbull.w2odata.com:9200",   
   // elasticsearch: 'http://localhost:9200',
   kibana_index:     "kibana-int", 
   modules:          ['histogram','map','pie','table','stringquery','sort',
